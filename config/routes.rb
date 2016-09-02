ddcca5c790eb7533e1b774a075631277f6cd6641
doctorq <qianhui_iwm@iwaimai.baidu.com>
2016-09-03 00:39:26 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/welcome'
+  get 'home/about'
+  get 'home/contact'
+  root 'products#index'
+  resources :products do
+  	collection do
+  		get :top # 排行榜功能
+  	end
+  	member do 
+  		post :buy # 添加购物车
+  	end
+    resources :variants
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
